d6c22a512eaf58786d71087cc0a4d1eb98e5de3c
lala7 <23DB013@okiu.ac.jp>
2025-03-12 19:09:14 +0900

    ボタンを作った

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ["tier-family.co.jp"], // 外部画像ドメインの許可
+    },
+  }
+  
+  module.exports = nextConfig
+  
