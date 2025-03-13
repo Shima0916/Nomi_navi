27f9355ca1db12de10f456245ae19dbc6633a6c2
Mutsuki Shimabukuro <e225733@ie.u-ryukyu.ac.jp>
2025-03-13 17:19:07 +0900

    delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ["tier-family.co.jp"], // 外部画像ドメインの許可
-    },
-  }
-  
-  module.exports = nextConfig
-  
